614856cfb4c05addb22ac27fac10fb2ea80225c6
tlseorb123 <tlseorb123@gmail.com>
2023-12-20 18:32:40 +0900

    04_텍스트파일.html

--- coupang/script.js
+++ coupang/script.js
@@ -12,27 +12,39 @@ navIcons[1].addEventListener("click", function () {
   });
 });
 
-// category
-const categoryBtn = document.querySelector(".category-btn");
-const category = document.querySelector(".category");
+// main
+const mainImg = document.querySelectorAll("main > img");
 
-categoryBtn.addEventListener("mouseenter", function () {
-  category.style.display = "flex";
-});
-category.addEventListener("mouseleave", function () {
-  category.style.display = "none";
-});
+// 첫번째 이미지에 main-img-active 클래스가 추가되어 있어요!
+setInterval(() => {
+  const className = "main-img-active";
+  for (let i = 0; i < mainImg.length; i++) {
+    // 첫번째 이미지에서는 클래스 제거, 두번째 이미지에 클래스 추가
+    // 두번째 이미지에서는 클래스 제거, 세번째 이미지에 클래스 추가
+    // 마지막 이미지에서는 클래스 제거, 첫번째 이미지에 클래스 추가
+    if (mainImg[i].classList.contains(className)) {
+      mainImg[i].classList.remove(className);
 
-const categoryItems = document.querySelectorAll(".category-item");
-const categorySub = document.querySelector(".category-sub");
-const categorySubItems = document.querySelectorAll(".category-sub-item");
+      // 마지막번째만 예외로 처리!
+      if (i === mainImg.length - 1) {
+        mainImg[0].classList.add(className);
+        break;
+      }
 
-categoryItems.forEach((item, index) => {
-  item.addEventListener("mouseenter", function () {
-    categorySub.style.display = "block";
-    categorySubItems[index].style.display = "block";
-  });
-  item.addEventListener("mouseleave", function () {
-    categorySubItems[index].style.display = "none";
-  });
+      mainImg[i + 1].classList.add(className);
+      break;
+    }
+  }
+}, 2000);
+
+// 사용자가 스크롤을 내리는 순간 어느 시점에 나타나고
+// 다시 올리면 사라지는..!
+const categoryMenu = document.querySelector(".category-menu");
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  if (window.scrollY >= 1300) {
+    categoryMenu.style.display = "flex";
+  } else {
+    categoryMenu.style.display = "none";
+  }
 });

d1671099cf74c61f5b59279b66aa925db2d6e143
tlseorb123 <152463286+tlseorb123@users.noreply.github.com>
2024-04-04 18:07:57 +0900

    front-and-workspace

--- test.js
+++ test.js
@@ -1,15 +1,5 @@
-function to() {
-  
-  // 토글 할 버튼 선택 (btn1)
-  const btn1 = document.querySelectorAll('#btn1');
-  
-  // btn1 숨기기 (display: none)
-  if(btn1.style.display !== 'none') {
-    btn1.style.display = 'none';
-  }
-  // btn` 보이기 (display: block)
-  else {
-    btn1.style.display = 'block';
-  }
-  
-}
+const men = document.querySelector("#men");
+
+men.addEventListener("click", function () {
+  men.style.display = "none";
+});

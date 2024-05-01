842f8c82f0a91d2984245739c6016f3852e056db
tlseorb123 <152463286+tlseorb123@users.noreply.github.com>
2024-05-01 17:53:31 +0900

    front-and-workspace

---
+++ vue-frontboard/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <PageHeader />
+  <!-- 헤더 컴포넌트 -->
+  <router-view />
+  <!-- 페이지 이동이 표시될 곳 -->
+  <PageFooter />
+  <!-- 푸터 컴포넌트 -->
+</template>
+
+<script>
+import PageFooter from "@/components/PageFooter.vue";
+import PageHeader from "@/components/PageHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageHeader,
+    PageFooter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-frontboard/src/views/PageHome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div calss="home">
+    <img alt="Vue logo" src="" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/Helloworld.vue";
+
+export default {
+  name: "PageHome",
+  components: {
+    HelloWorld,
+  },
+};
+</script>

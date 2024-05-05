0a20ef67efefa1acf5747489c6466562b0e3825d
tlseorb123 <152463286+tlseorb123@users.noreply.github.com>
2024-05-06 02:13:47 +0900

    proj1

--- proj1/src/components/common/Header.vue
+++ proj1/src/components/common/Header.vue
@@ -1,5 +1,23 @@
 <template>
     <header>
-        <h1><a href="#" class="logo"><img src="" alt=""></a></h1>
+        <h1><a href="#" class="logo"><img alt="Vue logo" src="../../components/static/image/blusky2.png" width="130"/></a></h1>
+        <div class="menuWrap">
+            <ul class="menu">
+                <li><a href="javascript:;">메뉴1</a></li>
+                <li><a href="javascript:;">메뉴2</a></li>
+                <li><a href="javascript:;">메뉴3</a></li>
+                <li><a href="javascript:;">메뉴4</a></li>
+            </ul>
+        </div>
     </header>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+header{width: 100%; text-align: center; position: relative; height: 120px; border-bottom: 1px solid #35495e;}
+header h1{position: absolute; top: 0; left: 100px;}
+</style>
